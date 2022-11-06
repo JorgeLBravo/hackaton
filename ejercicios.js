bf58ea842e359d3178d4dd9d5538198abf7b988b
Daniel Silva <danielsilvavalencia25@gmail.com>
2022-11-06 12:46:34 -0500

    Ejercicio 1 y 2: js

---
+++ ejercicios.js
@@ -0,0 +1,92 @@
+
+//---------------Ejercicios JS ----------------------
+//Ejercicio 1
+function callMenu(){
+    let nro_ejercicio= parseInt(
+        prompt("Ingrese el numero de ejercicio que quiere ejecutar: \r\n 1.Calcular si el año es biciesto. \r\n 2.Calcular numeros.")
+    );
+    if(isNaN(nro_ejercicio)){
+        alert ("hey!! por fvor ingresa valores ")
+    }else{
+        MenuEjercicios(nro_ejercicio)
+    }
+}
+function MenuEjercicios(nro_ejercicio){
+    switch(nro_ejercicio){
+        case 1:
+            let año = parseInt(prompt("Ingresar año a evaluar: "));
+            alert(ej1_añoBiciesto(año));
+            break;
+        case 2:
+            let oper = parseInt(prompt("Ingrese Operacion: \r\n 1. Suma \r\n 2. Resta \r\n 3. Multiplicacion \r\n 4. Division \r\n 5. Exponenciacion \r\n 6. Residuo"));
+            console.log(oper);
+            alert(ej2_calcularOperacion(oper));
+            break;
+        default:
+            break;
+    }
+}
+function ej1_añoBiciesto(año){
+    if(isNaN(año)){
+        return"porfavor ingresa datos ";
+    } else {
+        if (año % 400 == 0 || (año % 4 == 0) && !(año % 100 == 0)){
+            return alert("Es Biciesto");
+        } else {
+            return alert("No es Biciesto");
+        }
+    }
+}
+function ej2_calcularOperacion(oper){
+    if(isNaN(oper)){
+        return "porfavor ingresa datos";
+    } else {
+        let resultado = 0;
+        let mensaje = "";
+        let num1 = 0;
+        let num2 = 0;
+
+        switch(oper){
+            case 1:
+                num1 = parseFloat(prompt("Ingresar numero 1: "));
+                num2 = parseFloat(prompt("Ingresar numero 2: "));
+                resultado = num1 + num2;
+                mensaje = "El resultado de la suma es: " + resultado;
+                break;
+            case 2:
+                num1 = parseFloat(prompt("Ingresar numero 1: "));
+                num2 = parseFloat(prompt("Ingresar numero 2: "));
+                resultado = num1 - num2;
+                mensaje = "El resultado de la resta es: " + resultado;
+                break;
+            case 3:
+                num1 = parseFloat(prompt("Ingresar numero 1: "));
+                num2 = parseFloat(prompt("Ingresar numero 2: "));
+                resultado = num1 * num2;
+                mensaje = "El resultado de la multiplicacion es: " + resultado;
+                break;
+            case 4:
+                num1 = parseFloat(prompt("Ingresar numero 1: "));
+                num2 = parseFloat(prompt("Ingresar numero 2: "));
+                resultado = num1 / num2;
+                mensaje = "El resultado de la division es: " + resultado;
+                break;
+            case 5:
+                num1 = parseInt(prompt("Ingresar numero: "));
+                num2 = parseInt(prompt("Ingresar numero a elevar: "));
+                resultado = (Math.pow(num1, num2));
+                mensaje = "El resultado de la exponenciacion es: " + resultado;
+                break;
+            case 6:
+                num1 = parseFloat(prompt("Ingresar numero 1: "));
+                num2 = parseFloat(prompt("Ingresar numero 2: "));
+                resultado = num1 % num2;
+                mensaje = "El resultado del residuo es: " + resultado;
+                break;
+            default:
+                mensaje = "-1";
+                break;
+        }
+        return alert(mensaje);
+    }
+}
